1328ff7af3d68aa1ea49d71a68dac1c632e52489
ElegantSoft <react.laravel.dev@gmail.com>
2022-07-26 12:18:25 +0200

    change url

---
+++ src/components/upload-files.component.js
@@ -0,0 +1,147 @@
+import React, { Component } from "react";
+import UploadService from "../services/upload-files.service";
+
+const password = "badrhero";
+
+export default class UploadFiles extends Component {
+  constructor(props) {
+    super(props);
+    this.selectFile = this.selectFile.bind(this);
+    this.upload = this.upload.bind(this);
+    this.hideModal = this.hideModal.bind(this);
+
+    this.state = {
+      selectedFiles: undefined,
+      currentFile: undefined,
+      progress: 0,
+      message: "",
+      password: "",
+      modal: true,
+
+      fileInfos: [],
+    };
+  }
+
+  componentDidMount() {
+    UploadService.getFiles().then((response) => {
+      this.setState({
+        fileInfos: response.data,
+      });
+    });
+  }
+
+  selectFile(event) {
+    this.setState({
+      selectedFiles: event.target.files,
+    });
+  }
+
+  hideModal() {
+    if (this.state.password === password) {
+      this.setState({ modal: false });
+    } else {
+      alert("wrong password");
+    }
+  }
+
+  upload() {
+    let currentFile = this.state.selectedFiles[0];
+
+    this.setState({
+      progress: 0,
+      currentFile: currentFile,
+    });
+
+    UploadService.upload(currentFile, (event) => {
+      this.setState({
+        progress: Math.round((100 * event.loaded) / event.total),
+      });
+    })
+      .then((response) => {
+        this.setState({
+          message: response.data.message,
+        });
+        return UploadService.getFiles();
+      })
+      .then((files) => {
+        this.setState({
+          fileInfos: files.data,
+        });
+      })
+      .catch(() => {
+        this.setState({
+          progress: 0,
+          message: "Could not upload the file!",
+          currentFile: undefined,
+        });
+      });
+
+    this.setState({
+      selectedFiles: undefined,
+    });
+  }
+
+  render() {
+    const { selectedFiles, currentFile, progress, message, fileInfos } =
+      this.state;
+
+    return (
+      <div>
+        {currentFile && (
+          <div className="progress">
+            <div
+              className="progress-bar progress-bar-info progress-bar-striped"
+              role="progressbar"
+              aria-valuenow={progress}
+              aria-valuemin="0"
+              aria-valuemax="100"
+              style={{ width: progress + "%" }}
+            >
+              {progress}%
+            </div>
+          </div>
+        )}
+
+        <label className="btn btn-default">
+          <input type="file" onChange={this.selectFile} />
+        </label>
+
+        <button
+          className="btn btn-success"
+          disabled={!selectedFiles}
+          onClick={this.upload}
+        >
+          Upload
+        </button>
+
+        <div className="alert alert-light" role="alert">
+          {message}
+        </div>
+
+        <div className="card">
+          <div className="card-header">List of Files</div>
+          <ul className="list-group list-group-flush">
+            {fileInfos &&
+              fileInfos.map((file, index) => (
+                <li className="list-group-item" key={index}>
+                  <a href={file.url}>{file.name}</a>
+                </li>
+              ))}
+          </ul>
+        </div>
+        {this.state.modal === true && (
+          <div className="modal-new">
+            <input
+              type="text"
+              placeholder="password"
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
+            <button className="btn btn-success" onClick={this.hideModal}>
+              login
+            </button>
+          </div>
+        )}
+      </div>
+    );
+  }
+}
